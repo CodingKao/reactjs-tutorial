7485cd7a1bc1ba6a43909ffa31b1b09fa86b80bd
Kao Saelor <saelor.kao@gmail.com>
2023-06-27 12:47:26 -0700

    components types

---
+++ src/Components/Greet.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// Conventional way of writing a function
+// function Greet() {
+//     return <h1>Hello Kao!</h1>
+// }
+
+// ES6 way of writing a function
+const Greet = () => <h1>Hello Kao!</h1>
+
+export default Greet;
---
+++ src/Components/Welcome.js
@@ -0,0 +1,10 @@
+import { React, Component } from 'react';
+
+// create a class that extends Component
+class Welcome extends Component {
+    render() {
+        return <h1>Class Component</h1>
+    }
+}
+
+export default Welcome;
