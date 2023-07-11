a2e4e9efc5fc8e0eca2b3f42d4d5cce835c397fb
Kao Saelor <saelor.kao@gmail.com>
2023-07-10 22:41:27 -0700

    methods as props

--- src/App.js
+++ src/App.js
@@ -8,11 +8,13 @@ import Counter from './Components/Counter';
 import FunctionClick from './Components/FunctionClick';
 import ClassClick from './Components/ClassClick';
 import EventBind from './Components/EventBind';
+import ParentComponent from './Components/ParentComponent';
 
 
 function App() {
   return (
     <div className="App">
+      <ParentComponent />
       <EventBind /> 
       <FunctionClick />  
       <ClassClick />
