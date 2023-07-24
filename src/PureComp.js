a7cad2128de8962e939d3b89c05149fbe3a51baf
Kao Saelor <saelor.kao@gmail.com>
2023-07-24 09:03:11 -0700

    pure components

---
+++ src/PureComp.js
@@ -0,0 +1,13 @@
+import React, { PureComponet } from 'react';
+
+export class PureComp extends PureComponents {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default PureComp
